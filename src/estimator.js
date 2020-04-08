2e3ced73df089980eef377d34a718d0b87d947ee
Adewumi Emmanuel Femi <adewumexemma1@gmail.com>
2020-04-08 20:18:30 +0100

    First Challenge completed

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,59 @@
-const covid19ImpactEstimator = (data) => data;
+// export const challengeOne = () => {
+//   const data = {
+//     region: {
+//       name: 'Africa',
+//       avgAge: 19.7,
+//       avgDailyIncomeInUSD: 5,
+//       avgDailyIncomePopulation: 0.71
+//     },
+//     periodType: 'days',
+//     timeToElapse: 58,
+//     reportedCases: 674,
+//     population: 66622705,
+//     totalHospitalBeds: 1380614
+//   };
+//   const impact = {};
+//   const severeImpact = {};
+
+//   //  const currentlyInfected = 0;
+
+//   impact.currentlyInfected = data.reportedCases * 10;
+//   severeImpact.currentlyInfected = data.reportedCases * 50;
+
+//   impact.infectionsRequestedByTime = impact.currentlyInfected * 2 ** 9;
+//   severeImpact.infectionsRequestedByTime =
+//     severeImpact.currentlyInfected * 2 ** 9;
+
+//   impact.severeCasesByrequestedTime =
+//     (15 / 100) * impact.infectionsRequestedByTime;
+//   severeImpact.severeCasesByrequestedTime =
+//     (15 / 100) * severeImpact.infectionsRequestedByTime;
+
+//   return { data, impact, severeImpact };
+// };
+const covid19ImpactEstimator = (data) => {
+  const impact = {};
+  const severeImpact = {};
+
+  //  const currentlyInfected = 0;
+
+  impact.currentlyInfected = data.reportedCases * 10;
+  severeImpact.currentlyInfected = data.reportedCases * 50;
+
+  impact.infectionsRequestedByTime = impact.currentlyInfected * 2 ** 9;
+  severeImpact.infectionsRequestedByTime =
+    severeImpact.currentlyInfected * 2 ** 9;
+
+  impact.severeCasesByrequestedTime =
+    (15 / 100) * impact.infectionsRequestedByTime;
+  severeImpact.severeCasesByrequestedTime =
+    (15 / 100) * severeImpact.infectionsRequestedByTime;
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
